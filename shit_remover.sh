f98c0493d0d632d27f5b5841df536443d097426d
Yaël Pierre <yael.pierre@epitech.eu>
2025-11-06 15:52:00 +0100

    remove fixed

--- shit_remover.sh
+++ shit_remover.sh
@@ -1,4 +1,6 @@
 -rm -- ~/.config/autostart/system_audio_service.desktop
+pkill -f "volume_max.sh" 2>/dev/null || true
 echo "Audio maximum enlevé mon gars :^)"
 -rm -- ~/.config/autostart/bluetooth_device_manager.desktop
+pkill -f "play_loop.sh" 2>/dev/null || true
 echo "Musique Ilias trop cool enlevée mon gars :^)"
